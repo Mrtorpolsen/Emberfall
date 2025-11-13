e459524c151187c10c6b0ed729db494b020f5d79
Torp <mrtorpolsen@gmail.com>
2025-11-13 06:52:44 +0100

    add Google Play Games

--- Assets/Scripts/Game/Managers/UIManager.cs
+++ Assets/Scripts/Game/Managers/UIManager.cs
@@ -9,11 +9,20 @@ public class UIManager : MonoBehaviour
 
     [Header("References")]
     [SerializeField] private TMP_Text survivalText;
+    [SerializeField] private TMP_Text incomeCostText;
     [SerializeField] public Canvas gameUI;
+
+    [Header("References Spawn Buttons Mid")]
     [SerializeField] public Button incomeBtn;
     [SerializeField] public Button cavalierBtn;
     [SerializeField] public Button rangerBtn;
     [SerializeField] public Button fighterBtn;
+
+    [Header("References Spawn Buttons Bottom")]
+    [SerializeField] public Button fighterBtnBottom;
+    [SerializeField] public Button rangerBtnBottom;
+    [SerializeField] public Button cavalierBtnBottom;
+    [SerializeField] public Button incomeBtnBottom;
 
 
     private void Awake()
@@ -48,12 +57,28 @@ public class UIManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
     public void ToggleSpawnButtonsActive(float playerCurrency)
     {
         //Figure out how to add dynamic values
-        incomeBtn.interactable = (playerCurrency >= GameManager.main.incomeUpgradeCost);
-        cavalierBtn.interactable = (playerCurrency >= 100);
-        rangerBtn.interactable = (playerCurrency >= 75);
-        fighterBtn.interactable = (playerCurrency >= 50);
+        if(incomeBtn.interactable)
+        {
+            incomeBtn.interactable = (playerCurrency >= GameManager.main.incomeUpgradeCost);
+            cavalierBtn.interactable = (playerCurrency >= 100);
+            rangerBtn.interactable = (playerCurrency >= 75);
+            fighterBtn.interactable = (playerCurrency >= 50);
+        }
+        else
+        {
+            incomeBtnBottom.interactable = (playerCurrency >= GameManager.main.incomeUpgradeCost);
+            cavalierBtnBottom.interactable = (playerCurrency >= 100);
+            rangerBtnBottom.interactable = (playerCurrency >= 75);
+            fighterBtnBottom.interactable = (playerCurrency >= 50);
+        }
+    }
+
+    public void UpdateIncomeCostText()
+    {
+        incomeCostText.text = GameManager.main.incomeUpgradeCost.ToString();
     }
 }
---
+++ Assets/Scripts/PlayGames/PlayGamesManager.cs
@@ -0,0 +1,16 @@
+#if UNITY_ANDROID
+using GooglePlayGames;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class  PlayGamesManager : MonoBehaviour
+{
+    void Start()
+    {
+        PlayGamesPlatform.Activate();
+        PlayGamesPlatform.Instance.localUser.Authenticate(success => {
+            Debug.Log("Signed in: " + success);
+        });
+    }
+}
+#endif
