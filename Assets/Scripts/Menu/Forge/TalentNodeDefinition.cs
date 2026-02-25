29ccd7b74d82335eefbec21111eabd254b201382
Torp <mrtorpolsen@gmail.com>
2026-02-25 15:28:36 +0100

    moved talentnode to forge folder, add research cat def and add categorynode

---
+++ Assets/Scripts/Menu/Forge/TalentNodeDefinition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine.UIElements;
+
+public class TalentNodeDefinition
+{
+    public string img;
+    public string heading;
+    public string description;
+    public string purchased;
+    public int tier;
+    public float cost;
+    public PopupButtonDefinition buttonDefinition;
+    public Action onClick;
+
+    public Label purchasedLabel;
+    public Action<int, int> UpdatePurchasedText;
+}
---
+++ Assets/Scripts/Menu/Research/ResearchCategoryNodeDefinition.cs
@@ -0,0 +1,12 @@
+﻿public class ResearchCategoryNodeDefinition
+{
+    public bool isResearchActive;
+
+    //Inactive research
+    public string categoryName;
+
+    //Active research
+    public string researchName;
+    public string researchRank;
+    public string researchTimeLeft;
+}
