1cbe25eb280cf51d435e493d30c9b67467811df5
Torp <mrtorpolsen@gmail.com>
2026-05-15 11:10:09 +0200

    After Build V0.4.0

--- Assets/Scripts/Menu/Research/ResearchService.cs
+++ Assets/Scripts/Menu/Research/ResearchService.cs
@@ -4,7 +4,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
